53700314a8bbb62983a7ade5df2afbe670aa838f
Sriganesh Lokesh <sriganeshlokesh@Sriganeshs-MacBook-Pro.local>
2020-12-02 11:20:38 -0500

    Deployment Changes

--- index.js
+++
@@ -1,6 +0,0 @@
-const app = require("./server");
-
-// Start the server
-const port = process.env.PORT || 5000;
-app.listen(port);
-console.log(`Server Listening on Port: ${port}`);
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 const auth = require("./routes/api/auth");
 const budget = require("./routes/api/budget");
 const expense = require("./routes/api/expense");
+const PORT = process.env.PORT || 5000;
 
 // Body Parser Middleware
 app.use(express.json());
@@ -47,4 +48,19 @@ app.use("/api/auth", auth);
 app.use("/api/budget", budget);
 app.use("/api/expense", expense);
 
-module.exports = app;
+// Server Static Assets in Production
+if (process.env.NODE_ENV === "production") {
+  // Set Static Folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// Static Middleware
+app.use(express.static("public"));
+
+// Listening on Port 5000
+app.listen(PORT, (req, res) => {
+  console.log(`Server Running on Port: ${PORT}`);
+});
